c26ec622bb7199b9c04f02246ec1773b95e5bd7e
Krishna Prasad A <krishna.pa@pitsolutions.com>
2022-04-07 10:06:57 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'simple_ui_elements',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
